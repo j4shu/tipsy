b496fee25d7fa60672762dcfcd177b87c896ff14
jshu <jasonshu@terpmail.umd.edu>
2026-03-22 18:09:20 -0400

    remove comfy-table dependency and remove unused table implementation

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,24 @@
 use std::io::{self, Write};
 
-use comfy_table::Table;
-
 fn main() {
-    // let total = read_f64("Total:");
-    // let tax = read_f64("Tax:");
-    // let subtotal = total - tax;
-    let subtotal = 69.45;
+    let total = read_f64("Total:");
+    let tax = read_f64("Tax:");
+    let subtotal = total - tax;
     println!("Subtotal: {subtotal}");
-
-    // let mut table = Table::new();
-    // table.set_header(["Tip %", "Tip", "Total"]);
-    // for percentage in [0.15, 0.175, 0.20, 0.25] {
-    //     let tip = subtotal * percentage;
-    //     table.add_row([
-    //         format!("{:.2}%", percentage * 100.0),
-    //         format!("${:.2}", tip),
-    //         format!("${:.2}", subtotal + tip),
-    //     ]);
-    // }
-    // println!("Suggested tip amounts for a subtotal of ${subtotal}:");
-    // println!("{table}");
 
     let min_total = (subtotal * 1.15_f64).ceil() as u64;
     let max_total = (subtotal * 1.20_f64).floor() as u64;
 
-    let mut round_table = Table::new();
-    round_table.set_header(["Tip %", "Tip", "Total"]);
+    println!("{:<10} {:<10} {:<10}", "Tip %", "Tip", "Total");
     for total in min_total..=max_total {
         let tip = total as f64 - subtotal;
         let percentage = tip / subtotal * 100.0;
-        round_table.add_row([
-            format!("{:.2}%", percentage),
-            format!("${:.2}", tip),
-            format!("${total}.00"),
-        ]);
+        println!(
+            "{:<10} {:<10} ${total}.00",
+            format!("{percentage:.2}%"),
+            format!("${tip:.2}")
+        );
     }
-    println!("Round-total tips for a subtotal of ${subtotal}:");
-    println!("{round_table}");
 }
 
 fn read_f64(prompt: &str) -> f64 {
